4d7b449b961e21a400ce51986124e672325f6983
lehoangtruonghuy <MAY 06@LAB2-17>
2024-11-18 11:14:14 +0700

    Add project files.

---
+++ bai13_LuyThua.cpp
@@ -0,0 +1,21 @@
+﻿// bai13_LuyThua.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "TINH LUY THUA" << endl;
+
+    int so, mu, luythua=1;
+    cout << "nhap so: "; cin >> so;
+    cout << "nhap mu: "; cin >> mu;
+    for (int i = 0; i < mu; i++)
+    {
+        luythua *=so;
+    }
+    cout << so << " ^ " << mu << " = " << luythua;
+
+
+    return 0;
+}
